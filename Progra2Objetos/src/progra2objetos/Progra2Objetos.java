f67ef894ac3ebcb7eff62117926b284f60808f16
cristianmag1995 <cristianmag.27@gmail.com>
2015-06-11 01:11:27 -0600

    pass

--- Progra2Objetos/src/progra2objetos/Progra2Objetos.java
+++ Progra2Objetos/src/progra2objetos/Progra2Objetos.java
@@ -12,8 +12,8 @@ public class Progra2Objetos {
        
                 
         
-        Client nc1 = new Client("Cristian","Miguel","Male","c","b08c8c585b6d67164c163767076445d6", "b08c8c585b6d67164c163767076445d6",1,20,"Costa Rica",85794421,"colones");
-        Client nc2 = new Client("Kevin","Andres","Male","kevinandres.126@gmail.com","9d5e3ecdeb4cdb7acfd63075ae046672", "9d5e3ecdeb4cdb7acfd63075ae046672",2,20,"Mexico",87892400,"dollars");
+        Client nc1 = new Client("Cristian","Miguel","Male","c","b08c8c585b6d67164c163767076445d6", "cristian",1,20,"Costa Rica",85794421,"colones");
+        Client nc2 = new Client("Kevin","Andres","Male","kevinandres.126@gmail.com","9d5e3ecdeb4cdb7acfd63075ae046672", "kevin",2,20,"Mexico",87892400,"dollars");
         
         Globals.getInstance().setUserList(nc1);
         Globals.getInstance().setUserList(nc2);
